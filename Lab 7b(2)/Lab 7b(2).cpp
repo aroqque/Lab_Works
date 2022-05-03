8d7c1511aa352ed79c69dfecdd82b11bfe0dd757
Никита Куц <Nikita.Kus@cs.khpi.edu.ua>
2022-05-03 22:43:22 +0300

    completed

---
+++ Lab 7b(2)/Lab 7b(2).cpp
@@ -0,0 +1,27 @@
+﻿#include "foo.h"
+#define N 10
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+
+	char fname1[] = "text1.txt";
+
+	int arr1[N];
+	int arr2[N];
+	int x = 0;
+
+	fill_array(arr1, N);
+	cout << "Первый массив:\n";
+	print_array(arr1, N);
+	foo(fname1, arr1, N);
+	sum_foo(fname1, arr2, &x, N);
+
+	cout << "\n\nВторой массив:\n";
+	print_array(arr2, N);
+
+	cout << "\n\nСумма: " << x << endl;
+	system("pause");
+}
---
+++ Lab 7b(2)/foo.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+using namespace std;
+void fill_array(int*, const int);
+void print_array(int*, const int);
+void foo(char*, int*, const int);
+void sum_foo(char*, int*, int*, const int);
