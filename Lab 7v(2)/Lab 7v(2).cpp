de9a994ac587f86e5befb26ca812b71e92ae2dcb
Никита Куц <Nikita.Kus@cs.khpi.edu.ua>
2022-05-04 00:29:47 +0300

    completed

---
+++ Lab 7v(2)/Lab 7v(2).cpp
@@ -0,0 +1,25 @@
+﻿#include "foo.h"
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+	int x = 0;
+	char fname[] = "text.bin";
+
+	input(fname);
+	cout << "Вывод первого файла:\n";
+	read(fname);
+	x = sum(fname);
+	cout << "\n\n\n";
+	cout << "Сумма = " << x;
+	cout << "\n\n\n";
+	sort(fname);
+	cout << "\n";
+	cout << "Вывод второго файла с замененным минимальным значением:\n";
+	read(fname);
+	cout << "\n\n\n";
+	system("pause");
+
+}
